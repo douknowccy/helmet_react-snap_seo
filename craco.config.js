63ac81bfe1c028b16cf73a4e6a15327507a88ab4
Falco <twkccy1990@gmail.com>
2022-04-02 23:24:56 +0800

    using github unMerge react-snap version

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,1 @@
-const path = require("path");
-// 載入 prerender-spa-plugin (第一步)
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-// 載入 PuppeteerRenderer (第二步)
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
-
 module.exports = {};
